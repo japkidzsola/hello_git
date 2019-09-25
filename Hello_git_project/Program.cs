3a27c31d3205cf7c4bedd8d0684d3537fa301c69
Zsolti <suzukipontzsolt@gmail.com>
2019-09-25 22:10:49 +0200

    a jól elkészült feladat

--- Hello_git_project/Program.cs
+++ Hello_git_project/Program.cs
@@ -18,18 +18,18 @@ namespace Hello_git_project
             Console.WriteLine("Kérem adja meg mennnyi számot tartalmazzon a tömb.");
             darabszam = Convert.ToInt32(Console.ReadLine());
 
-            
+
                 for (int i = 0; i < darabszam; i++)
                 {
                     Console.WriteLine("Adja meg a következő számot");
                     szam = Convert.ToInt32(Console.ReadLine());
                     Lista.Add(szam);
-
+                }
                 int max = Lista.Max();
                 int min = Lista.Min();
                 Console.WriteLine("A legnagyobb értékű szám:  " + max);
                 Console.WriteLine("A legkissebb értékű szám:  " + min);
-                }
+                
             }catch(Exception e)
             {
                 Console.WriteLine("A hiba: " + e);
